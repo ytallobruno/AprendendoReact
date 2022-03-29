3e5a9cb46445946a8c9308b20f5882289c9467f4
Ytallo Bruno <ytallobruno@hotmail.com>
2022-03-29 14:01:12 -0300

    update UL

--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -6,7 +6,7 @@ class ListaDeNotas extends Component {
   render() {
     return (
       <ul className="lista-notas">
-        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
+        {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
               <CardNota />
