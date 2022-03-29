e103fa61e97b8c65f56427afd1c85604c7a7f2f8
Ytallo Bruno <ytallobruno@hotmail.com>
2022-03-29 14:38:42 -0300

    add constructor, state, props

--- src/App.js
+++ src/App.js
@@ -5,11 +5,29 @@ import "./assets/App.css";
 import './assets/index.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = { 
+      notas: [] 
+    };
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novoArrayNotas
+    }
+    this.setState(novoEstado);
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
