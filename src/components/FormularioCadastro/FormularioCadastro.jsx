8e143175844e8c2349079ad0dd84bcf4016ac19d
Ytallo Bruno <ytallobruno@hotmail.com>
2022-03-29 13:03:05 -0300

    add constructor and handle's

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 import "./style.css";
 
 class FormularioCadastro extends Component {
+  constructor() {
+    super();
+    this.titulo = "";
+    this.texto = "";
+  }
+
+  handleMudancaTitulo(evento) {
+    this.titulo = evento.target.value;
+  }
+
+  handleMudancaTexto(evento) {
+    this.texto = evento.target.value;
+  }
+
   render() {
     return (
       <form className="form-cadastro ">
@@ -9,11 +23,13 @@ class FormularioCadastro extends Component {
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this.handleMudancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota"
           className="form-cadastro_input"
+          onChange={this.handleMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
